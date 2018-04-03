7c67cef1e96097de7f5d195699cf59cd5eb238e5
angeld <roman.andriyanov@corp.mail.ru>
2018-04-03 17:42:01 +0300

    cpp

--- bld.bat
+++ bld.bat
@@ -2,4 +2,4 @@
 cd build
 call "C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools_msbuild.bat" 
 
-msbuild hello.sln  /t:Build /p:Configuration=Release
+msbuild opengl.sln  /t:Build /p:Configuration=Release
